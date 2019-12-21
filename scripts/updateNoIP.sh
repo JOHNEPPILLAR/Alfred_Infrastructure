ad30c7c4b6366b571e8306b774ddeca1a1b11d71
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-21 21:45:58 +0000

    added docker build

--- scripts/updateNoIP.sh
+++ scripts/updateNoIP.sh
@@ -5,5 +5,5 @@ echo "Run the container"
 cd ../noip
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down --rmi all
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d --build
 cd ..
