0c14e080310175ffece6447cc8d185406a0f0ae4
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-21 21:55:25 +0000

    updated folder name

--- scripts/updateNoIP.sh
+++ scripts/updateNoIP.sh
@@ -2,7 +2,7 @@
 clear
 
 echo "Run the container"
-cd ../noip
+cd ../NoIP
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down --rmi all
 docker-compose -f docker-compose.yml up -d --build
