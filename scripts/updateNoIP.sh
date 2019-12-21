2e55070ff046b406613b8c6957bc5e9bac1221de
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-21 20:49:33 +0000

    Added restart

--- scripts/updateNoIP.sh
+++ scripts/updateNoIP.sh
@@ -9,5 +9,5 @@ docker rm -f noip
 
 echo "Run updated container"
 cd ../NoIP
-docker run --name=noip -d -v /etc/localtime:/etc/localtime -v /config/dir/path:/config coppit/no-ip
+docker run -d --restart=always --name=noip -v /etc/localtime:/etc/localtime -v /config/dir/path:/config coppit/no-ip
 cd ..
