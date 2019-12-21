e9c9762c254073d43fb3fd75604e331eca3fd1ec
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-21 21:18:25 +0000

    renamed folder

--- scripts/updateNoIP.sh
+++ scripts/updateNoIP.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 clear
 
-echo "Get latest container"
-docker pull coppit/no-ip
-
 echo "Run the container"
-cd ../NoIP
+cd ../noip
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down --rmi all
 docker-compose -f docker-compose.yml up -d
