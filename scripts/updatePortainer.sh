0f906b91cba3208f321dfe3c3cc22618259588e3
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-21 18:34:41 +0000

    added portainer and registery

---
+++ scripts/updatePortainer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Pull latest image"
+docker pull portainer/portainer
+
+echo "Remove old image"
+docker stop portainer
+docker rm portainer
+docker rmi portainer
+
+echo "Run image"
+docker run -d --restart=always --name=portainer -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
