4c2cb6232c12b3bcc255d9238e774f47f7e13c02
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-22 11:48:24 +0000

    Added remote server to read

--- scripts/updateTimeScaleDB.sh
+++ scripts/updateTimeScaleDB.sh
@@ -5,7 +5,7 @@ echo "Login to vault"
 vault login -address=$VAULT_URL $VAULT_TOKEN
 
 echo "Set env vars"
-VAULES=$(vault read -format=json secret/alfred/production)
+VAULES=$(vault read -address=$VAULT_URL -format=json secret/alfred/production)
 DATA_STORE_USER_PASSWORD=$(echo $VAULES | jq .data.DataStoreUserPassword)
 
 echo "Run the container"
