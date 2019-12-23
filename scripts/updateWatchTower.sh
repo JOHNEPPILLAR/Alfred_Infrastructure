211bc8b0409c2df4346f9044802b2af29779e048
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-23 19:33:50 +0000

    removing " from url

--- scripts/updateWatchTower.sh
+++ scripts/updateWatchTower.sh
@@ -6,7 +6,7 @@ vault login -address=$VAULT_URL $VAULT_TOKEN
 echo "Set env vars"
 VAULES=$(vault read -address=$VAULT_URL -format=json secret/alfred/production)
 SLACK_WEB_HOOK=$(echo $VAULES | jq .data.SlackWebHook)
-export SLACK_WEB_HOOK=$(echo $SLACK_WEB_HOOK//\"}
+export SLACK_WEB_HOOK=$(echo "${SLACK_WEB_HOOK:1:-1}")
 
 echo "Run the container"
 cd ../watchtower
