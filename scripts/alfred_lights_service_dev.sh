bd2441e49197eb26190b9cc2b2a56e8a1a95d374
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-26 21:47:58 +0000

    removed docker login

--- scripts/alfred_lights_service_dev.sh
+++ scripts/alfred_lights_service_dev.sh
@@ -7,7 +7,6 @@ export MOCK="true"
 
 echo "Run the container"
 cd ../alfred_lights_service
-echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
 docker-compose -f docker-compose.dev.yml pull
 docker-compose -f docker-compose.dev.yml down --rmi all
 docker-compose -f docker-compose.dev.yml up -d
--- scripts/alfred_netatmo_data_collector_service_dev.sh
+++ scripts/alfred_netatmo_data_collector_service_dev.sh
@@ -7,7 +7,6 @@ export MOCK="true"
 
 echo "Run the container"
 cd ../alfred_netatmo_data_collector_service
-echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down --rmi all
 docker-compose -f docker-compose.yml up -d
