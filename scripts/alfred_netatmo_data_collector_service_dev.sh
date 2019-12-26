c2798355e2a99236f9f62b181502c96ca5181286
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-26 21:41:31 +0000

    added netatmo

--- scripts/alfred_netatmo_data_collector_service_dev.sh
+++ scripts/alfred_netatmo_data_collector_service_dev.sh
@@ -6,9 +6,9 @@ export ENVIRONMENT="development"
 export MOCK="true"
 
 echo "Run the container"
-cd ../alfred_lights_service
+cd ../alfred_netatmo_data_collector_service
 echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
-docker-compose -f docker-compose.dev.yml pull
-docker-compose -f docker-compose.dev.yml down --rmi all
-docker-compose -f docker-compose.dev.yml up -d
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml down --rmi all
+docker-compose -f docker-compose.yml up -d
 cd ..
