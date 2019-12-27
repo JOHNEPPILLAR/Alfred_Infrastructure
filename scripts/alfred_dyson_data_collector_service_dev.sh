73cb926d3709e34c17b44b87b4414fe6e8c31c6e
JOHNEPPILLAR <johneppillar@gmail.com>
2019-12-27 12:07:24 +0000

    added dyson service

---
+++ scripts/alfred_dyson_data_collector_service_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+
+echo "Set env vars"
+export ENVIRONMENT="development"
+export MOCK="true"
+
+echo "Run the container"
+cd ../alfred_dyson_data_collector_service
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml down --rmi all
+docker-compose -f docker-compose.yml up -d
+cd ..
