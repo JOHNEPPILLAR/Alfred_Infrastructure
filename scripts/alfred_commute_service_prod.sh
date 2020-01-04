ddace1086282f41c7f8eafe41c80cc5052e01fb4
JOHNEPPILLAR <johneppillar@gmail.com>
2020-01-04 11:13:03 +0000

    Added commute service

---
+++ scripts/alfred_commute_service_prod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+
+echo "Set env vars"
+export ENVIRONMENT="production"
+export MOCK="false"
+
+echo "Run the container"
+cd ../alfred_commute_service
+echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml down --rmi all
+docker-compose -f docker-compose.yml up -d
+cd ..
