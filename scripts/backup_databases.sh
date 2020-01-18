b85dae4502966e542942e689b9f31ca88dbae50e
JOHNEPPILLAR <johneppillar@gmail.com>
2020-01-18 19:47:44 +0000

    added new databases to backups

--- scripts/backup_databases.sh
+++ scripts/backup_databases.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 echo "Backing up databases"
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/devices.bak devices -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/lights.bak lights -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/schedules.bak schedules -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/arlo.bak devices -U postgres'
 docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/commute.bak commute -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/devices.bak devices -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/dyson.bak devices -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/flowercare.bak devices -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/lights.bak lights -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/netatmo.bak lights -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/schedules.bak schedules -U postgres'
+docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/tmplink.bak schedules -U postgres'
 
 echo "Copy bak files to local storage"
-cp /var/lib/docker/volumes/timescaledb_data/_data/devices.bak /home/jp/DockerBackup/TimeScaleDB
-cp /var/lib/docker/volumes/timescaledb_data/_data/lights.bak /home/jp/DockerBackup/TimeScaleDB/
-cp /var/lib/docker/volumes/timescaledb_data/_data/schedules.bak /home/jp/DockerBackup/TimeScaleDB/
-cp /var/lib/docker/volumes/timescaledb_data/_data/commute.bak /home/jp/DockerBackup/TimeScaleDB/
+cp /var/lib/docker/volumes/timescaledb_data/_data/* /home/jp/DockerBackup/TimeScaleDB
 
 echo "Copy bak files to longterm storage"
 cp /home/jp/DockerBackup/TimeScaleDB/* /mnt/local_share/Current/TimeScaleDB
