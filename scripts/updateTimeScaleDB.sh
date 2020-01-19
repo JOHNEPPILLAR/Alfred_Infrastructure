86120d15d557b5992a3b2bc9fb38d9469df4e5d0
JOHNEPPILLAR <johneppillar@gmail.com>
2020-01-19 20:23:08 +0000

    rem oved " from env

--- scripts/updateTimeScaleDB.sh
+++ scripts/updateTimeScaleDB.sh
@@ -6,7 +6,8 @@ vault login -address=$VAULT_URL $VAULT_TOKEN
 
 echo "Set env vars"
 VAULES=$(vault read -address=$VAULT_URL -format=json secret/alfred/production)
-export DATA_STORE_USER_PASSWORD=$(echo $VAULES | jq .data.DataStoreUserPassword)
+DATA_STORE_USER_PASSWORD=$(echo $VAULES | jq .data.DataStoreUserPassword)
+export DATA_STORE_USER_PASSWORD=$(echo "${DATA_STORE_USER_PASSWORD:1:-1}")
 
 echo "Run the container"
 cd ../timescale_db
