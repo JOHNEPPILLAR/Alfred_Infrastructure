973d5b13c139d3d604cdc7584b47f62f0c689836
JOHNEPPILLAR <johneppillar@gmail.com>
2020-01-19 17:46:37 +0000

    added update scripts

---
+++ scripts/alfred_reverse_proxy_prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+
+echo "Run the container"
+cd ../alfred_reverse_proxy_prod
+
+cp -r ~/DockerScripts/Certs/. ./volumes/certs/
+
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml down --rmi all
+docker-compose -f docker-compose.yml up -d
+cd ..
