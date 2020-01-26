fe672831c0424e385dc74c14d17a42d2e6667b24
JOHNEPPILLAR <johneppillar@gmail.com>
2020-01-26 13:51:35 +0000

    fixed incorrect ip addr

--- scripts/alfred_controller_service_prod.sh
+++ scripts/alfred_controller_service_prod.sh
@@ -8,7 +8,7 @@ export PORT=3978
 export ALFRED_FLOWERCARE_SERVICE="https://kidsroomserver:3981"
 export ALFRED_COMMUTE_SERVICE="https://alfred_commute_service:3978"
 export ALFRED_NETATMO_SERVICE="https://alfred_netatmo_data_collector_service:3978"
-export ALFRED_DYSON_SERVICE="https://alfred_dyson_data_collector_service:3978"
+export ALFRED_DYSON_SERVICE="https://192.168.1.7:3982"
 export ALFRED_LIGHTS_SERVICE="https://alfred_lights_service:3978"
 
 echo "Run the container"
