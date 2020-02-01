9a3a251a6574e408dd94e33df031586d89e5c8ef
JOHNEPPILLAR <johneppillar@gmail.com>
2020-02-01 17:00:11 +0000

    swapped name to ip

--- scripts/alfred_controller_service_prod.sh
+++ scripts/alfred_controller_service_prod.sh
@@ -5,7 +5,7 @@ echo "Set env vars"
 export ENVIRONMENT="production"
 export MOCK="false"
 export PORT=3978
-export ALFRED_FLOWERCARE_SERVICE="https://kidsroomserver:3981"
+export ALFRED_FLOWERCARE_SERVICE="https://192.168.1.7:3981"
 export ALFRED_COMMUTE_SERVICE="https://alfred_commute_service:3978"
 export ALFRED_NETATMO_SERVICE="https://alfred_netatmo_data_collector_service:3978"
 export ALFRED_DYSON_SERVICE="https://192.168.1.7:3982"
