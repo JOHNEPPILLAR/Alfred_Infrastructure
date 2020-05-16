8e0fb106b94c1834de2ad2c1e4a44922edbda8d6
JOHNEPPILLAR <johneppillar@gmail.com>
2020-05-16 20:51:20 +0100

    Updated proxy configs

--- scripts/alfred_flowercare_data_collector_service_dev.sh
+++ scripts/alfred_flowercare_data_collector_service_dev.sh
@@ -9,9 +9,10 @@ export PORT=3978
 echo "Run the container"
 cd ../alfred_flowercare_data_collector_service
 echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml down --rmi all
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose-dev.yml pull
+docker-compose -f docker-compose-dev.yml down --rmi all
+docker-compose -f docker-compose-dev.yml up -d
 docker logout
 cd ..
 docker restart reverse_proxy
+docker image prune -f
--- scripts/alfred_reverse_proxy_dev.sh
+++ scripts/alfred_reverse_proxy_dev.sh
@@ -8,5 +8,6 @@ cp -r ~/Code/Alfred/Certs/localhost/. ./volumes/certs/
 
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down --rmi all
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d --build
 cd ..
+docker image prune -f
