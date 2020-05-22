46c5b0f611f5b42630d9301ad849d8f4992ac4cb
JOHNEPPILLAR <johneppillar@gmail.com>
2020-05-22 17:08:33 +0100

    Updated folder paths

--- scripts/alfred_flowercare_data_collector_service_rpi.sh
+++ scripts/alfred_flowercare_data_collector_service_rpi.sh
@@ -7,11 +7,11 @@ export MOCK="false"
 export PORT=3981
 
 echo "Get latest code"
-cd /Alfred_FlowerCare_Data_Collector_Service
+cd ../../Alfred_FlowerCare_Data_Collector_Service
 git pull
 
 echo "Move scripts into location"
-cp ../..//Alfred_Infrastructure/alfred_flowercare_data_collector_service/* .
+cp ../../Alfred_Infrastructure/alfred_flowercare_data_collector_service/* .
 
 echo "Run the container"
 docker-compose -f docker-compose-rpi.yml down --rmi all
