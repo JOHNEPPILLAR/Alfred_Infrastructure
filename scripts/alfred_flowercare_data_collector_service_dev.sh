417d341c8a6ca1bd3e7c83bb6d9af46c7b083ca8
JOHNEPPILLAR <johneppillar@gmail.com>
2020-05-23 15:59:40 +0100

    swap remove them pull order

--- scripts/alfred_flowercare_data_collector_service_dev.sh
+++ scripts/alfred_flowercare_data_collector_service_dev.sh
@@ -9,8 +9,8 @@ export PORT=3978
 echo "Run the container"
 cd ../alfred_flowercare_data_collector_service
 echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
-docker-compose -f docker-compose-dev.yml pull
 docker-compose -f docker-compose-dev.yml down --rmi all
+docker-compose -f docker-compose-dev.yml pull
 docker-compose -f docker-compose-dev.yml up -d
 docker logout
 cd ..
--- scripts/alfred_flowercare_data_collector_service_prod.sh
+++ scripts/alfred_flowercare_data_collector_service_prod.sh
@@ -9,8 +9,8 @@ export PORT=3981
 echo "Run the container"
 cd ../alfred_flowercare_data_collector_service
 echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
-docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down --rmi all
+docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
 docker logout
 cd ..
