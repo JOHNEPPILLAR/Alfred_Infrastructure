725def1a89c4c695df341168313ff6d82740d214
JOHNEPPILLAR <johneppillar@gmail.com>
2020-05-24 21:56:32 +0100

    added pull do doc reg script

--- scripts/updateRegistry.sh
+++ scripts/updateRegistry.sh
@@ -4,5 +4,6 @@ clear
 echo "Run the container"
 cd ../registry
 docker-compose -f docker-compose.yml down --rmi all
+docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
 cd ..
