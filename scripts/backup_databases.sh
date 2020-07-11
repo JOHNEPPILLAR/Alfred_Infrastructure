7ff186cab764fb3403d8b9a49f6cc9029deb0789
JOHNEPPILLAR <johneppillar@gmail.com>
2020-07-11 14:49:53 +0100

    fixed bst timexone and backup copy script

--- scripts/backup_databases.sh
+++ scripts/backup_databases.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "Backing up databases"
-docker exec -it timescaledb bash -c 'pg_basebackup -l "Backup created at $(hostname) on $(date)" -U postgres -D /mnt/local_share/Current/TimeScaleDB'
+echo "Back up databases"
+docker exec -it timescaledb bash -c 'rm -rf /var/lib/postgresql/data/backup'
+docker exec -it timescaledb bash -c 'mkdir /var/lib/postgresql/data/backup'
+docker exec -it timescaledb bash -c 'pg_basebackup -l "Backup created at $(hostname) on $(date)" -U postgres -D /var/lib/postgresql/data/backup'
+
+echo "Copy to long time storage"
+cp -r /dockerVolumes/timescale_db_data/_data/backup /mnt/local_share/Current/TimeScaleDB
