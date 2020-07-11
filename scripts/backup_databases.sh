67efb080558393a0a31817ef03e0c202b74f8919
JOHNEPPILLAR <johneppillar@gmail.com>
2020-07-11 14:18:15 +0100

    Only perform backup, let host clean folder

--- scripts/backup_databases.sh
+++ scripts/backup_databases.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-echo "Clear backup folder"
-docker exec -it timescaledb bash -c 'rm -rf /mnt/local_share/Current/TimeScaleDB *'
-
 echo "Backing up databases"
 docker exec -it timescaledb bash -c 'pg_basebackup -l "Backup created at $(hostname) on $(date)" -U postgres -D /mnt/local_share/Current/TimeScaleDB'
