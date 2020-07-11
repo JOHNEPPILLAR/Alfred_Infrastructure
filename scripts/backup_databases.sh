6916678a9985182c1fbc4264eae85c2de7778053
JOHNEPPILLAR <johneppillar@gmail.com>
2020-07-11 14:02:37 +0100

    Update timeScale backup script

--- scripts/backup_databases.sh
+++ scripts/backup_databases.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
+echo "Clear backup folder"
+docker exec -it timescaledb bash -c 'rm -rf /mnt/local_share/Current/TimeScaleDB *'
+
 echo "Backing up databases"
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/arlo.bak arlo -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/commute.bak commute -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/devices.bak devices -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/dyson.bak dyson -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/flowercare.bak flowercare -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/lights.bak lights -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/netatmo.bak netatmo -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/schedules.bak schedules -U postgres'
-docker exec -it timescaledb bash -c 'pg_dump -Fc -f /var/lib/postgresql/data/tplink.bak tplink -U postgres'
-
-echo "Copy bak files to local storage"
-cp /var/lib/docker/volumes/timescale_db_data/_data/*.bak /home/jp/DockerBackup/TimeScaleDB
-
-echo "Copy bak files to longterm storage"
-cp /home/jp/DockerBackup/TimeScaleDB/* /mnt/local_share/Current/TimeScaleDB
+docker exec -it timescaledb bash -c 'pg_basebackup -l "Backup created at $(hostname) on $(date)" -U postgres -D /mnt/local_share/Current/TimeScaleDB'
