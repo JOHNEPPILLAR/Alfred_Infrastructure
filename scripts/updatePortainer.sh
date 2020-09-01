37eccef2e7f94cc12bc1b9fa1b2bbebdbc4c0710
John Pillar <jp@Johns-MacBook-Pro.local>
2020-09-01 09:49:40 +0100

    Update to v2

--- scripts/updatePortainer.sh
+++ scripts/updatePortainer.sh
@@ -9,4 +9,4 @@ docker rm portainer
 docker rmi portainer
 
 echo "Run image"
-docker run -d --restart=always --name=portainer -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d --restart=always --name=portainer -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
