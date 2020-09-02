78e30982f6f93adf69f4ff5005d327c1b56b7f80
John Pillar <jp@Johns-MacBook-Pro.local>
2020-09-02 22:27:13 +0100

    Update certs path

--- scripts/updateRegistry.sh
+++ scripts/updateRegistry.sh
@@ -3,7 +3,8 @@ clear
 
 echo "Run the container"
 cd ../registry
-cp ~/DockerScripts/Certs/* ~/DockerScripts/Alfred_Infrastructure/registery/certs/.
+cp ~/DockerScripts/Certs/* ~/DockerScripts/Alfred_Infrastructure/registry/certs/.
+
 docker-compose -f docker-compose.yml down --rmi all
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
