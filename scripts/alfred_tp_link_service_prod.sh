b3d3ec5e9ea875dbaf017b838661e7939b672e04
John Pillar <jp@Johns-MacBook-Pro.local>
2020-09-05 12:34:42 +0100

    Update weather service ur

--- scripts/alfred_tp_link_service_prod.sh
+++ scripts/alfred_tp_link_service_prod.sh
@@ -5,7 +5,7 @@ echo "Set env vars"
 export ENVIRONMENT="production"
 export MOCK="false"
 export PORT=3984
-export ALFRED_WEATHER_SERVICE="https://alfred_weather_service:3978"
+export ALFRED_WEATHER_SERVICE="https://alfred_weather_service:3981/weather"
 export TRACE_LEVEL="debug"
 
 echo "Run the container"
