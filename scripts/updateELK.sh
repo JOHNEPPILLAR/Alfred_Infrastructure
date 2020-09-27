17577bb20d56395ecb1d27204615e937d970e98e
JP <johneppillar@gmail.com>
2020-09-27 11:27:38 +0100

    Remove un-used scripts

--- scripts/updateELK.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-
-echo "Run the container"
-cd ../elk
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d
-cd ..
--- scripts/updateFileBeats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-
-echo "Run the container"
-cd ../filebeats
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d --build
-cd ..
