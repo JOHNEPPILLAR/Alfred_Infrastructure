386394d048574f15a2b2366690c293628ab4c633
JP <johneppillar@gmail.com>
2020-10-04 12:02:47 +0100

    Add tidyup after pull

--- watchtower/setup.sh
+++ watchtower/setup.sh
@@ -8,9 +8,13 @@ VAULES=$(vault read -address=$VAULT_URL -format=json secret/alfred/production)
 SLACK_WEB_HOOK=$(echo $VAULES | jq .data.SlackWebHook)
 export SLACK_WEB_HOOK=$(echo "${SLACK_WEB_HOOK:1:-1}")
 
-echo "Run the container"
-cd ../watchtower
+echo "Runing the container..."
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d --build
-cd ..
+docker logout
+
+echo "Purge docker images..."
+docker image prune -f
+
+echo "Finished"
