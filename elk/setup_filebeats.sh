a23661f732892368c2c3e7a36fd42be9f07a2e7c
JP <johneppillar@gmail.com>
2020-10-30 20:53:44 +0000

    Add filebeats

--- elk/setup_filebeats.sh
+++ elk/setup_filebeats.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 clear
 
-echo "Setup/run elk docker container"
-echo "------------------------------"
+echo "Setup/run filebeats docker container"
+echo "------------------------------------"
 echo ""
 
 export ELK_VERSION=7.9.3
+export ELK_HOST=192.168.1.12:9200
 
 echo "Runing the container..."
 docker-compose -f docker-compose.yml down
@@ -14,7 +15,5 @@ docker-compose -f docker-compose.yml up -d --build
 
 echo "Purge docker images..."
 docker image prune -f
-
-./setup_filebeats.sh
 
 echo "Finished"
