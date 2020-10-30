c99789718a29aa21a1d115ff57a7a5a6edff82be
JP <johneppillar@gmail.com>
2020-10-30 20:55:40 +0000

    Fix path

--- elk/setup_filebeats.sh
+++ elk/setup_filebeats.sh
@@ -9,9 +9,9 @@ export ELK_VERSION=7.9.3
 export ELK_HOST=192.168.1.12:9200
 
 echo "Runing the container..."
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d --build
+docker-compose -f filebeats/docker-compose.yml down
+docker-compose -f filebeats/docker-compose.yml pull
+docker-compose -f filebeats/docker-compose.yml up -d --build
 
 echo "Purge docker images..."
 docker image prune -f
