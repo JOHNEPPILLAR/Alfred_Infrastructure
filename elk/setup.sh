89ef00505aec1d370db26ae84953e4df9f7ca6ff
JP <johneppillar@gmail.com>
2020-10-31 16:34:01 +0000

    Update fonfig

--- elk/setup.sh
+++ elk/setup.sh
@@ -9,13 +9,12 @@ export ELK_VERSION=7.9.3
 export ELK_HOST=192.168.1.12:9200
 
 echo "Runing the container..."
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d --build
+docker-compose down
+docker-compose pull
+docker-compose build filebeats
+docker-compose up -d
 
 echo "Purge docker images..."
 docker image prune -f
-
-./setup_filebeats.sh
 
 echo "Finished"
