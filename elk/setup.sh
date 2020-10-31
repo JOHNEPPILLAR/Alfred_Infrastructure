5ea558828b4fca8117bf710a8ffbd87b02e44e3b
JP <johneppillar@gmail.com>
2020-10-31 19:58:14 +0000

    set config file permissions

--- elk/setup.sh
+++ elk/setup.sh
@@ -8,10 +8,34 @@ echo ""
 export ELK_VERSION=7.9.3
 export ELK_HOST=192.168.1.12:9200
 
-echo "Runing the container..."
-docker-compose down
-docker-compose pull
-docker-compose up -d
+echo "Set permissions on config file"
+chown root filebeat/config/filebeat.yml
+chmod go-w filebeat/config/filebeat.yml
+
+SETUP_ELK="$1"
+if [ -z "$SETUP_ELK" ]
+then
+    echo ""
+    echo "Setup ELK or just Filebeats?"
+    select SETUP_ELK in "ELK" "Filebeats";
+    do
+        break
+    done
+fi
+
+echo ""
+case $SETUP_ELK in
+    ELK )   echo "Setup policies..."
+            docker-compose down
+            docker-compose pull
+            docker-compose up -d
+            ;;
+    Filebeats ) echo "Skipping vault setup"
+                docker-compose down filebeats
+                docker-compose pull filebeats
+                docker-compose up -d filebeats
+                ;;
+esac
 
 echo "Purge docker images..."
 docker image prune -f
