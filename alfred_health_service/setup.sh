9ec1084da1fddd9721496c7111448bcbb824d851
JP <johneppillar@gmail.com>
2020-11-14 10:43:28 +0000

    Refactor lights service

--- alfred_health_service/setup.sh
+++ alfred_health_service/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-echo "Setup/run alfred lights service docker container"
+echo "Setup/run alfred health service docker container"
 echo "------------------------------------------------"
 echo ""
 
@@ -43,33 +43,27 @@ fi
 echo ""
 case $SETUP_VAULT in
     Yes )   echo "Setup policies..."
-            vault policy write -address=$VAULT_URL alfred_lights_service policy.hcl
-            vault write -address=$VAULT_URL auth/approle/role/alfred_lights_service_role token_ttl=1m token_max_ttl=2m token_policies=alfred_lights_service
-  
-            echo "Storing config..."
-            read -p "Hue Bridge User: " HueBridgeUser
-            vault write -address=$VAULT_URL secret/alfred_lights_service/HueBridgeUser data=$HueBridgeUser
-            read -p "Hue Bridge IP: " HueBridgeIP
-            vault write -address=$VAULT_URL secret/alfred_lights_service/HueBridgeIP data=$HueBridgeIP
+            vault policy write -address=$VAULT_URL alfred_health_service policy.hcl
+            vault write -address=$VAULT_URL auth/approle/role/alfred_health_service_role token_ttl=1m token_max_ttl=2m token_policies=alfred_health_service  
             ;;
     No )    echo "Skipping vault setup"
             ;;
 esac
 
 echo "Creating certs..."
-mkcert alfred_lights_service
+mkcert alfred_health_service
 
 echo "Storing certs..."
-vault write -address=$VAULT_URL secret/alfred_lights_service/ssl_key data=@alfred_lights_service-key.pem
-vault write -address=$VAULT_URL secret/alfred_lights_service/ssl_cert data=@alfred_lights_service.pem
+vault write -address=$VAULT_URL secret/alfred_health_service/ssl_key data=@alfred_health_service-key.pem
+vault write -address=$VAULT_URL secret/alfred_health_service/ssl_cert data=@alfred_health_service.pem
 echo "Tidying up certs..."
 rm *.pem
 
 echo "Creating access token..."
-VAULES=$(vault read -address=$VAULT_URL -format=json auth/approle/role/alfred_lights_service_role/role-id)
+VAULES=$(vault read -address=$VAULT_URL -format=json auth/approle/role/alfred_health_service_role/role-id)
 APP_ROLE_ID=$(echo $VAULES | jq .data.role_id)
 export APP_ROLE_ID=${APP_ROLE_ID:1:${#APP_ROLE_ID}-2}
-VAULES=$(vault write -f --format=json -address=$VAULT_URL auth/approle/role/alfred_lights_service_role/secret-id)
+VAULES=$(vault write -f --format=json -address=$VAULT_URL auth/approle/role/alfred_health_service_role/secret-id)
 APP_TOKEN=$(echo $VAULES | jq .data.secret_id)
 export APP_TOKEN=${APP_TOKEN:1:${#APP_TOKEN}-2}
 
@@ -77,7 +71,7 @@ echo "Runing the container..."
 echo $DOCKER_REGISTERY_PASSWORD | docker login $DOCKER_REGISTERY_URL -u $DOCKER_REGISTERY_USERNAME --password-stdin 
 docker-compose down
 docker-compose pull
-docker-compose up -d
+docker-compose up -d --build
 docker logout
 
 echo "Purge docker images..."
